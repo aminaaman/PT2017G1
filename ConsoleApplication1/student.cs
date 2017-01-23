9331d0446cf1f1e1fcc086f2f9727fa72fc6dbbd
aminaaman <aminaaman07@gmail.com>
2017-01-23 07:04:35 +0600

    student

---
+++ ConsoleApplication1/student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public double gpa;
+
+        public Student(string name, string surname, int age, double gpa)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + age + " " + gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Amina", "Aman", 25, 3.8);
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
